6faec2b78d3ba95caaa5b7f331c735209c806022
Kunal Dabir <kunal.dabir@gmail.com>
2018-11-12 22:29:55 +0530

    simplest possible try catch expression

--- src/index.ts
+++ src/index.ts
@@ -2,8 +2,19 @@ interface Mapper<T, U> {
   (i: T): U
 }
 
+interface Generator<T> {
+  (): T
+}
+
+
 export function $ifelse<C, R>($condition: C, $then: Mapper<C, R>, $else: Mapper<C, R>): R {
   return $condition ? $then($condition) : $else($condition);
 }
 
-
+export function $trycatch<T, E>($try: Generator<T>, $catch: Mapper<E, T>) {
+  try {
+    return $try()
+  } catch (e) {
+    return $catch(e);
+  }
+}
--- test/index.spec.ts
+++ test/index.spec.ts
@@ -1,4 +1,5 @@
 import {$ifelse} from "../src";
+import {$trycatch} from "../src";
 
 it("$ifelse", () => {
 
@@ -9,4 +10,21 @@ it("$ifelse", () => {
 
   expect(foo).toBe("FOO");
   expect(bar).toBe("_");
+});
+
+it("$trycatch", () => {
+
+  const fnThatMayThrowException = (n: number) => {
+    if (n % 2 === 0) {
+      return n * 2;
+    } else {
+      throw new Error("bad luck");
+    }
+  };
+
+  const success = $trycatch(() => fnThatMayThrowException(2), e => 0);
+  const failure = $trycatch(() => fnThatMayThrowException(1), e => 0);
+
+  expect(success).toBe(4);
+  expect(failure).toBe(0);
 });
